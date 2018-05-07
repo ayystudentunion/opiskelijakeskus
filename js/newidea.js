d637aef74a619c2ae3dce25805a160554bcc8f6f
Zentryn <zentryn@gmail.com>
2018-05-07 15:43:41 +0300

    Added automatic generation of ideas for easier testing

--- js/newidea.js
+++ js/newidea.js
@@ -1,7 +1,10 @@
 var fs = require('browserify-fs');
 const $ = require('jquery');
+const randWord = require('random-words');
 
 var sendIdeaBtn = document.getElementById('send_idea_btn');
+var generateIdeas = document.getElementById('generate_ideas');
+
 var iconsContainer = document.getElementById('icons');
 var imagesFolderName = "images/";
 var selectedIcon = null;
@@ -64,14 +67,50 @@ sendIdeaBtn.onclick = function() {
         newIdeaObj[ideaName]["description"] = ideaDesc;
         newIdeaObj[ideaName]["reasons"] = ideaReasons;
         newIdeaObj[ideaName]["icon"] = iconFile;
+        newIdeaObj[ideaName]["likes"] = "0";
 
         result.push(newIdeaObj);
 
         $.ajax({
-            url: '../php/saveNewIdea.php',
+            url: '../php/save_data.php',
             type: 'POST',
             data: {"file_path": "../data/data.json", "data": JSON.stringify(result)},
             success: function(data) {}
         });
+    });
+}
+
+generateIdeas.onclick = function() {
+    var ideasCount = parseInt(document.getElementById('how_many_random_ideas').value);
+
+    var ideas = [];
+    for (var i = 0; i < ideasCount; i++) {
+        var idea = {};
+        var name = randWord();
+        name = name.charAt(0).toUpperCase() + name.slice(1);
+        var desc = "";
+        for (var j = 0; j < Math.random() * 500; j++) {
+            var word = randWord();
+            if (j == 0) word = word.charAt(0).toUpperCase() + word.slice(1);
+            desc += word + " ";
+        }
+
+        desc.charAt(0)
+
+        idea[name] = {};
+        idea[name].description = desc;
+        idea[name].likes = String(Math.floor(Math.random() * 200));
+        idea[name].icon = "icon" + String(Math.floor(Math.random() * 6) + 1) + ".png";
+
+        ideas.push(idea);
+    }
+
+    console.log(ideas);
+
+    $.ajax({
+        url: '../php/save_data.php',
+        type: 'POST',
+        data: {"file_path": "../data/data.json", "data": JSON.stringify(ideas)},
+        success: function(data) {}
     });
 }
