668a1173e57a2ee032456ff285aed7d1a430a4b5
Zentryn <zentryn@gmail.com>
2018-05-07 15:45:17 +0300

    Renamed php file and added locking of file to prevent data loss

---
+++ php/save_data.php
@@ -0,0 +1,17 @@
+<?php
+    if (isset($_POST['file_path']) && isset($_POST['data'])) {
+        function save_data($file_path, $data) {
+            $handle = fopen($file_path, 'w+');
+
+            // Lock file to prevent data from getting fucked
+            if (flock($handle, LOCK_EX)) {
+                fwrite($handle, $data);
+                flock($fp, LOCK_UN);
+            }
+
+            fclose($handle);
+        }
+
+        save_data($_POST['file_path'], $_POST['data']);
+    }
+?>
