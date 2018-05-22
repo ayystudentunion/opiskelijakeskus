49c5e1703fda88743e2b3337cac8b86762a3bb7f
Zentryn <zentryn@gmail.com>
2018-05-22 16:05:00 +0300

    Added new script to check if the user is using Internet Explorer and show a different view if they are

---
+++ js/check_for_ie.js
@@ -0,0 +1,11 @@
+
+// Internet Explorer 6-11
+var isBrowserIE = /*@cc_on!@*/false || !!document.documentMode;
+
+if (isBrowserIE) {
+    document.getElementById('site-container').innerHTML = "";
+    document.getElementById('ie-message').classList.remove('no-display');
+    document.body.style.backgroundColor = "#033649";
+    document.body.style.overflowY = "hidden";
+    document.getElementById('ie-message-text-title').style.color = "white";
+}
