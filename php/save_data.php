8f768500c79efb958de9028472e618c851099cfc
Zentryn <veikkis98@gmail.com>
2018-05-29 01:20:02 +0300

    Fixed lock

--- php/save_data.php
+++ php/save_data.php
@@ -1,12 +1,12 @@
 <?php
     if (isset($_POST['file_path']) && isset($_POST['data'])) {
         function save_data($file_path, $data) {
-            $handle = fopen($file_path, 'w+');
+            $handle = fopen($file_path, 'r+');
 
             // Lock file to prevent data from getting fucked
             if (flock($handle, LOCK_EX)) {
                 fwrite($handle, $data);
-                flock($fp, LOCK_UN);
+                flock($handle, LOCK_UN);
             }
 
             fclose($handle);
