da47ac1209f37e843f02c668d937d14322bec07c
Zentryn <zentryn@gmail.com>
2018-06-06 17:15:22 +0300

    Moved cookie code here and added new function for updating translation of element

--- js/globals.js
+++ js/globals.js
@@ -37,3 +37,41 @@ function setCopyrightText() {
         copyrightTextEl.innerHTML = "Copyright © " + yearText + " <a href='https://ayy.fi' target='_blank'>Aalto University Student Union</a>";
     }
 }
+
+// Sets a new cookie
+function setCookie(cname, cvalue) {
+    // Store cookie for 1 year
+    var expiration_date = new Date();
+    expiration_date.setFullYear(expiration_date.getFullYear() + 1);
+
+    document.cookie = cname + "=" + cvalue + ";" + " expires=" + expiration_date.toUTCString();
+}
+
+// Get an existing cookie by name
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+// Updates element's text according to the current language
+function translateElement(el) {
+    var lang = getCookie("lang");
+    if (lang == "fi" || lang == "") return;
+
+    if (lang == "en" && el.hasAttribute('translation-en')) {
+        el.innerHTML = el.getAttribute('translation-en');
+    } else if (lang == "se" && el.hasAttribute('translation-se')) {
+        el.innerHTML = el.getAttribute('translation-se');
+    }
+}
