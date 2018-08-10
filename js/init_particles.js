b34bf806365bff64d741c30a271b8ac8ef467ade
Zentryn <zentryn@gmail.com>
2018-08-10 13:43:08 +0300

    Documentation

--- js/init_particles.js
+++ js/init_particles.js
@@ -1,15 +1,27 @@
+/**
+ * @file Initializes ParticlesJS
+ * @author Zentryn <https://github.com/Zentryn>
+ * 
+ * @see https://vincentgarreau.com/particles.js
+ */
+
+// Initialize ParticleJS
 function initParticles() {
     // Remove elements that are in the way
-    var footer = document.getElementById('page-footer');
-    if (footer != undefined) footer.classList.add('no-display');
+    {
+        var footer = document.getElementById('page-footer');
+        if (footer != undefined) footer.classList.add('no-display');
 
-    var spaceTakers = document.getElementsByClassName('bottom-spacetaker');
-    if (spaceTakers.length != 0) spaceTakers[0].classList.add('no-display');
+        var spaceTakers = document.getElementsByClassName('bottom-spacetaker');
+        if (spaceTakers.length != 0) spaceTakers[0].classList.add('no-display');
+    }
 
+    // Show particles container
     var particlesContainer = document.getElementById('particles-js');
     if (particlesContainer == undefined) return;
     particlesContainer.classList.remove('no-display');
 
+    // Custom options for nicer particles
     particlesJS("particles-js", {
         "particles": {
             "number": {
